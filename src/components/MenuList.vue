1ac151edd9b70a030bff4a21ab01ab98185bc06e
Mizu <psdk55052004@gmail.com>
2022-10-19 10:29:55 +0800

    新增選單+手風琴功能

--- src/components/MenuList.vue
+++ src/components/MenuList.vue
@@ -1,30 +1,284 @@
 <template>
-  選單
+  <aside>
+      <div class="accordion" id="accordionExample">
+        <!-- 快速功能 -->
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button py-2" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+              快速功能區
+            </button>
+          </h2>
+          <div id="collapse1" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div class="accordion-body overflow-scroll h-70">
+              <ul>
+                <li><a href="javascript:;">選單1</a></li>
+                <li>
+                  <a href="javascript:;">選單2</a>
+                  <ul>
+                    <li><a href="javascript:;">選單1</a></li>
+                    <li><a href="javascript:;">選單2</a></li>
+                    <li><a href="javascript:;">選單3</a></li>
+                  </ul>
+                </li>
+                <li><a href="javascript:;">選單3</a></li>
+                <li><a href="javascript:;">選單4</a></li>
+                <li>
+                  <a href="javascript:;">選單5</a>
+                  <ul>
+                    <li><a href="javascript:;">選單1</a></li>
+                    <li><a href="javascript:;">選單2</a></li>
+                    <li><a href="javascript:;">選單3</a></li>
+                  </ul>
+                </li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <!-- 課程進度 -->
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button py-2" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="true" aria-controls="collapse2">
+              課程進度
+            </button>
+          </h2>
+          <div id="collapse2" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div class="accordion-body overflow-scroll h-70">
+              <ul>
+                <li><a href="javascript:;">選單1</a></li>
+                <li>
+                  <a href="javascript:;">選單2</a>
+                  <ul>
+                    <li><a href="javascript:;">選單1</a></li>
+                    <li><a href="javascript:;">選單2</a></li>
+                    <li><a href="javascript:;">選單3</a></li>
+                  </ul>
+                </li>
+                <li><a href="javascript:;">選單3</a></li>
+                <li><a href="javascript:;">選單4</a></li>
+                <li>
+                  <a href="javascript:;">選單5</a>
+                  <ul>
+                    <li><a href="javascript:;">選單1</a></li>
+                    <li><a href="javascript:;">選單2</a></li>
+                    <li><a href="javascript:;">選單3</a></li>
+                  </ul>
+                </li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+                <li><a href="javascript:;">選單</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <!-- 課程功能 -->
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button py-2" type="button" data-bs-toggle="collapse" data-bs-target="#collapse3" aria-expanded="true" aria-controls="collapse3">
+              課程功能
+            </button>
+          </h2>
+          <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div class="accordion-body overflow-scroll h-70">
+              <ul>
+                <li><a href="javascript:;">選單1</a></li>
+                <li>
+                  <a href="javascript:;">選單2</a>
+                  <ul>
+                    <li><a href="javascript:;">選單1</a></li>
+                    <li><a href="javascript:;">選單2</a></li>
+                    <li><a href="javascript:;">選單3</a></li>
+                  </ul>
+                </li>
+                <li><a href="javascript:;">選單3</a></li>
+                <li><a href="javascript:;">選單4</a></li>
+                <li>
+                  <a href="javascript:;">選單5</a>
+                  <ul>
+                    <li><a href="javascript:;">選單1</a></li>
+                    <li><a href="javascript:;">選單2</a></li>
+                    <li><a href="javascript:;">選單3</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <!-- 課程功能 -->
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button py-2" type="button" data-bs-toggle="collapse" data-bs-target="#collapse4" aria-expanded="true" aria-controls="collapse4">
+              課程功能
+            </button>
+          </h2>
+          <div id="collapse4" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div class="accordion-body overflow-scroll h-70">
+              <ul>
+                <li><a href="javascript:;">選單1</a></li>
+                <li>
+                  <a href="javascript:;">選單2</a>
+                  <ul>
+                    <li><a href="javascript:;">選單1</a></li>
+                    <li><a href="javascript:;">選單2</a></li>
+                    <li><a href="javascript:;">選單3</a></li>
+                  </ul>
+                </li>
+                <li><a href="javascript:;">選單3</a></li>
+                <li><a href="javascript:;">選單4</a></li>
+                <li>
+                  <a href="javascript:;">選單5</a>
+                  <ul>
+                    <li><a href="javascript:;">選單1</a></li>
+                    <li><a href="javascript:;">選單2</a></li>
+                    <li><a href="javascript:;">選單3</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <!-- 課程功能 -->
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button py-2" type="button" data-bs-toggle="collapse" data-bs-target="#collapse5" aria-expanded="true" aria-controls="collapse5">
+              課程功能
+            </button>
+          </h2>
+          <div id="collapse5" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div class="accordion-body overflow-scroll h-70">
+              <ul>
+                <li><a href="javascript:;">選單1</a></li>
+                <li>
+                  <a href="javascript:;">選單2</a>
+                  <ul>
+                    <li><a href="javascript:;">選單1</a></li>
+                    <li><a href="javascript:;">選單2</a></li>
+                    <li><a href="javascript:;">選單3</a></li>
+                  </ul>
+                </li>
+                <li><a href="javascript:;">選單3</a></li>
+                <li><a href="javascript:;">選單4</a></li>
+                <li>
+                  <a href="javascript:;">選單5</a>
+                  <ul>
+                    <li><a href="javascript:;">選單1</a></li>
+                    <li><a href="javascript:;">選單2</a></li>
+                    <li><a href="javascript:;">選單3</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <!-- 課程功能 -->
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button py-2" type="button" data-bs-toggle="collapse" data-bs-target="#collapse6" aria-expanded="true" aria-controls="collapse6">
+              課程功能
+            </button>
+          </h2>
+          <div id="collapse6" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div class="accordion-body overflow-scroll h-70">
+              <ul>
+                <li><a href="javascript:;">選單1</a></li>
+                <li>
+                  <a href="javascript:;">選單2</a>
+                  <ul>
+                    <li><a href="javascript:;">選單1</a></li>
+                    <li><a href="javascript:;">選單2</a></li>
+                    <li><a href="javascript:;">選單3</a></li>
+                  </ul>
+                </li>
+                <li><a href="javascript:;">選單3</a></li>
+                <li><a href="javascript:;">選單4</a></li>
+                <li>
+                  <a href="javascript:;">選單5</a>
+                  <ul>
+                    <li><a href="javascript:;">選單1</a></li>
+                    <li><a href="javascript:;">選單2</a></li>
+                    <li><a href="javascript:;">選單3</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+  </aside>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+aside {
+  width: 30%;
+  height: calc(100vh - 50px);
 }
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.h-70 {
+  height: 70vh;
 }
 </style>
